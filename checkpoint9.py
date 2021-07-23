66c4dc7f409df112f067f702fa6e66fc6230e603
youssefneff <86619828+youssefneff@users.noreply.github.com>
2021-07-23 03:15:40 +0100

    Add files via upload

---
+++ checkpoint9.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#question 1
+def binary_search(item_list,item):
+	first = 0
+	last = len(item_list)-1
+	found = False
+	while( first<=last and not found):
+		mid = (first + last)//2
+		if item_list[mid] == item :
+			found = True
+		else:
+			if item < item_list[mid]:
+				last = mid - 1
+			else:
+			    first = mid + 1	
+	return found
+	
+print("binary_search([1,2,3,5,8], 6)->",binary_search([1,2,3,5,8], 6))
+print("binary_search([1,2,3,5,8], 5)->",binary_search([1,2,3,5,8], 5))
+
+
+# In[5]:
+
+
+#question 2
+def power(a,b):
+	if b==0:
+		return 1
+	elif a==0:
+		return 0
+	elif b==1:
+		return a
+	else:
+		return a*power(a,b-1)
+
+print("power(3,4)->",power(3,4))
+
+
+# In[6]:
+
+
+def bubbleSort(nlist):
+    for passnum in range(len(nlist)-1,0,-1):
+        for i in range(passnum):
+            if nlist[i]>nlist[i+1]:
+                temp = nlist[i]
+                nlist[i] = nlist[i+1]
+                nlist[i+1] = temp
+
+nlist = [29,13,22,37,52,49,46,71,56]
+bubbleSort(nlist)
+print(nlist)
+
+
+# In[7]:
+
+
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                nlist[k]=lefthalf[i]
+                i=i+1
+            else:
+                nlist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            nlist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            nlist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",nlist)
+
+nlist = [29,13,22,37,52,49,46,71,56]
+mergeSort(nlist)
+print(nlist)
+
+
+# In[8]:
+
+
+def quickSort(data_list):
+   quickSortHlp(data_list,0,len(data_list)-1)
+
+def quickSortHlp(data_list,first,last):
+   if first < last:
+
+       splitpoint = partition(data_list,first,last)
+
+       quickSortHlp(data_list,first,splitpoint-1)
+       quickSortHlp(data_list,splitpoint+1,last)
+
+
+def partition(data_list,first,last):
+   pivotvalue = data_list[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and data_list[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while data_list[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = data_list[leftmark]
+           data_list[leftmark] = data_list[rightmark]
+           data_list[rightmark] = temp
+
+   temp = data_list[first]
+   data_list[first] = data_list[rightmark]
+   data_list[rightmark] = temp
+
+
+   return rightmark
+
+data_list = [29,13,22,37,52,49,46,71,56]
+quickSort(data_list)
+print(data_list)
+
+
+# In[ ]:
+
+
+
+
